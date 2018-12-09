9191f6938c7d9f522eb43d11f3da02f5ec19edc5
Jeger <jegerq@gmail.com>
2018-12-09 23:28:48 +0200

    Added FeedBackDAL with methods: GetFeedBacks,GetFeedback By Goods Id and SortFeedBacks and SearchFeedBack by user login

---
+++ DAL/FeedBackDAL.cs
@@ -0,0 +1,67 @@
+﻿using AutoMapper;
+using DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TradeCompany.Database;
+
+namespace DAL
+{
+	public class FeedBackDAL : companyEntities
+	{
+		IMapper _mapper;
+		public FeedBackDAL()
+		{
+			var config = new AutoMapper.MapperConfiguration(c => c.AddProfiles(typeof(FeedBackDAL)));
+			_mapper = config.CreateMapper();
+		}
+
+		public List<FeedBackDTO> GetFeedBacks()
+		{
+			List<FeedBackDTO> FedBack = new List<FeedBackDTO>();
+
+			using (var entities = new CategoryDAL())
+			{
+				FedBack = _mapper.Map<List<FeedBackDTO>>(entities.tblFeedbacks).ToList();
+			}
+			return FedBack;
+		}
+
+
+
+		public List<FeedBackDTO> GetFeedbackByGoodsId(int GoodId)
+		{
+			List<FeedBackDTO> FedBack = new List<FeedBackDTO>();
+			using (var entities = new FeedBackDAL())
+			{
+
+				var feed = entities.tblFeedbacks.Where(s => s.good_id == GoodId).ToList();
+
+				return _mapper.Map<List<FeedBackDTO>>(feed);
+			}
+
+		}
+
+
+
+
+		public List<FeedBackDTO> SortFeedBacks(List<FeedBackDTO> feeds, int type)
+		{
+
+
+			switch (type)
+			{
+				case 1: return feeds.OrderBy(f => f.tblUser.first_name).ToList();
+				case 2: return (feeds.OrderBy(f => f.Date).ToList());
+				default:
+					return feeds;
+			}
+		}
+		public List<FeedBackDTO> SearchFeedBackLogin(List<FeedBackDTO> feeds, string name)
+		{
+			return feeds.Where(u => u.tblUser.first_name.Contains(name)).ToList();
+		}
+	}
+}
