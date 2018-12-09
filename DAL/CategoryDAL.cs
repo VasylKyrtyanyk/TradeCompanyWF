ac55c4ca99907ac2679f3ca31aa00b358002dc6a
Jeger <jegerq@gmail.com>
2018-12-09 23:20:59 +0200

    Added CategoryDAL with methdos : GetCategories,GetCategory by id

---
+++ DAL/CategoryDAL.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using DTO;
+using System.Collections.Generic;
+using System.Linq;
+using TradeCompany.Database;
+
+namespace DAL
+{
+	public class CategoryDAL : companyEntities
+	{
+
+		IMapper _mapper;
+
+		public CategoryDAL()
+		{
+			var config = new AutoMapper.MapperConfiguration(c => c.AddProfiles(typeof(CategoryDAL)));
+			_mapper = config.CreateMapper();
+		}
+
+		public List<CategoryDTO> GetCategories()
+		{
+			List<CategoryDTO> category = new List<CategoryDTO>();
+
+			using (var entities = new CategoryDAL())
+			{
+				category = _mapper.Map<List<CategoryDTO>>(entities.tblCategories).ToList();
+			}
+			return category;
+		}
+
+
+		public CategoryDTO GetCategoryById(int id)
+		{
+			using (var entities = new CategoryDAL())
+			{
+				var cat = entities.tblCategories.FirstOrDefault(c => c.id == id);
+				return _mapper.Map<CategoryDTO>(cat);
+			}
+		}
+
+	}
+}
