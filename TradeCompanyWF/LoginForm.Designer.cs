5281396355169aeb257a1b3bcc097266eafbff46
Jeger <jegerq@gmail.com>
2018-12-10 15:17:41 +0200

    Added : EmalTextBox and PassTextBox

--- TradeCompanyWF/LoginForm.Designer.cs
+++ TradeCompanyWF/LoginForm.Designer.cs
@@ -28,12 +28,59 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
+			this.EmailTextBox = new System.Windows.Forms.TextBox();
+			this.PassTextBox = new System.Windows.Forms.TextBox();
+			this.LoginButton = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// EmailTextBox
+			// 
+			this.EmailTextBox.Location = new System.Drawing.Point(146, 106);
+			this.EmailTextBox.Name = "EmailTextBox";
+			this.EmailTextBox.Size = new System.Drawing.Size(100, 20);
+			this.EmailTextBox.TabIndex = 0;
+			// 
+			// PassTextBox
+			// 
+			this.PassTextBox.Location = new System.Drawing.Point(146, 132);
+			this.PassTextBox.Name = "PassTextBox";
+			this.PassTextBox.Size = new System.Drawing.Size(100, 20);
+			this.PassTextBox.TabIndex = 1;
+			// 
+			// LoginButton
+			// 
+			this.LoginButton.Location = new System.Drawing.Point(160, 158);
+			this.LoginButton.Name = "LoginButton";
+			this.LoginButton.Size = new System.Drawing.Size(75, 23);
+			this.LoginButton.TabIndex = 2;
+			this.LoginButton.Text = "Login";
+			this.LoginButton.UseVisualStyleBackColor = true;
+			this.LoginButton.Click += new System.EventHandler(this.LoginButton_Click);
+			// 
+			// LoginForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "LoginForm";
+			this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.ClientSize = new System.Drawing.Size(384, 261);
+			this.Controls.Add(this.LoginButton);
+			this.Controls.Add(this.PassTextBox);
+			this.Controls.Add(this.EmailTextBox);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximumSize = new System.Drawing.Size(400, 300);
+			this.MinimumSize = new System.Drawing.Size(400, 300);
+			this.Name = "LoginForm";
+			this.Text = "Login";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.TextBox EmailTextBox;
+		private System.Windows.Forms.TextBox PassTextBox;
+		private System.Windows.Forms.Button LoginButton;
 	}
 }
--- TradeCompanyWF/LoginForm.cs
+++ TradeCompanyWF/LoginForm.cs
@@ -16,5 +16,10 @@ namespace TradeCompanyWF
 		{
 			InitializeComponent();
 		}
+
+		private void LoginButton_Click(object sender, EventArgs e)
+		{
+
+		}
 	}
 }
