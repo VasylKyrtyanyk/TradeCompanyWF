7f4fd1063865f4534c7c0aed4d55e3f84711591d
Jeger <jegerq@gmail.com>
2018-12-10 15:34:40 +0200

    Added MenuForm

--- TradeCompanyWF/LoginForm.cs
+++ TradeCompanyWF/LoginForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BussniessLogic.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,14 +13,41 @@ namespace TradeCompanyWF
 {
 	public partial class LoginForm : Form
 	{
-		public LoginForm()
+		protected IEntityBL _entityBL;
+
+		public LoginForm(IEntityBL entityBL)
 		{
 			InitializeComponent();
+			_entityBL = entityBL;
+			EmailTextBox.Text = "admin@com.ua";
+			PassTextBox.Text = "admin";
 		}
 
 		private void LoginButton_Click(object sender, EventArgs e)
 		{
+			if (String.IsNullOrWhiteSpace(EmailTextBox.Text) || String.IsNullOrWhiteSpace(PassTextBox.Text))
+			{
+				MessageBox.Show("You must enter password and login", "Warning!!!");
+				EmailTextBox.Text = "";
+				PassTextBox.Text = "";
+				return;
+			}
+			else
+			{
+				var res = _entityBL.Login(EmailTextBox.Text, PassTextBox.Text);
 
+				var user = _entityBL.GetUserById(res);
+
+				if (user != null)
+				{
+					var MarketForm = Program.Container.Resolve<MenuForm>(new ParameterOverride("user", user));
+					DialogResult = DialogResult.OK;
+					MarketForm.ShowDialog();
+					this.Close();
+					return;
+				}
+			}
+			MessageBox.Show("Incorrect password or login!!!", "Error");
 		}
 	}
 }
---
+++ TradeCompanyWF/MenuForm.cs
@@ -0,0 +1,27 @@
+﻿using BussniessLogic.Interfaces;
+using DTO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TradeCompanyWF
+{
+	public partial class MenuForm : Form
+	{
+		protected UserDTO _user;
+		protected IEntityBL _entityBL;
+
+		public MenuForm(IEntityBL entityBL, UserDTO user)
+		{
+			InitializeComponent();
+			_user = user;
+			_entityBL = entityBL;
+		}
+	}
+}
